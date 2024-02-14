aaae28b70a0faf6be22f486aa15f28c563ec6234
Cihan Mert PEKER <119748061+Cihanmertpeker@users.noreply.github.com>
2024-02-14 17:04:09 +0300

    commit

---
+++ CarBook.WebUI/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public  IActionResult Index()
+        {
+           
+            return View();
+        }      
+
+    }
+}
