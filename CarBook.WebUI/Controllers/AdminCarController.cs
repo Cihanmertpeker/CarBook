61d746a8d2dd0718f6c9b7023170da87d643eeee
Cihan Mert PEKER <119748061+Cihanmertpeker@users.noreply.github.com>
2024-02-16 21:39:54 +0300

    admin layout done

---
+++ CarBook.WebUI/Controllers/AdminCarController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Text;
+using CarBook.Dto.CarDtos;
+
+namespace UdemyCarBook.WebUI.Controllers
+{
+    public class AdminCarController : Controller
+    {
+        
+        public  IActionResult Index()
+        {
+           
+            return View();
+        }
+       
+    }
+}
