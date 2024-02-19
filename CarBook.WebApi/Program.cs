73e4059ea2d993200674ae67d9e5cdeb123f9a7f
Cihan Mert PEKER <119748061+Cihanmertpeker@users.noreply.github.com>
2024-02-19 17:57:30 +0300

    Commit

--- CarBook.WebApi/Program.cs
+++ CarBook.WebApi/Program.cs
@@ -20,6 +20,8 @@ using CarBook.Application.Features.RepositoryPattern;
 using CarBook.Persistence.Repositories.CommentRepositories;
 using CarBook.Application.Interfaces.StatisticsInterfaces;
 using CarBook.Persistence.Repositories.StatisticsRepositories;
+using CarBook.Application.Interfaces.RentACarInterfaces;
+using CarBook.Persistence.Repositories.RentACarRepositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,6 +35,7 @@ builder.Services.AddScoped(typeof(IBlogRepository), typeof(BlogRepository));
 builder.Services.AddScoped(typeof(ICarPricingRepository), typeof(CarPricingRepository));
 builder.Services.AddScoped(typeof(ITagCloudRepository), typeof(TagCloudRepository));
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(CommentRepository<>));
+builder.Services.AddScoped(typeof(IRentACarRepository), typeof(RentACarRepository));
 
 
 builder.Services.AddScoped<GetAboutQueryHandler>();
--- CarBook.WebUI/Controllers/DefaultController.cs
+++ CarBook.WebUI/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.LocationDtos;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
@@ -14,11 +15,36 @@ namespace CarBook.WebUI.Controllers
         }
 
         [HttpGet]
-        public  IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-           
+          
+                var client = _httpClientFactory.CreateClient();
+               
+                var responseMessage = await client.GetAsync("https://localhost:7102/api/Locations");
+
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
+                List<SelectListItem> values2 = (from x in values
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.Name,
+                                                    Value = x.LocationID.ToString()
+                                                }).ToList();
+                ViewBag.v = values2;
+            
             return View();
-        }      
+        }
+
+        [HttpPost]
+        public IActionResult Index(string book_pick_date, string book_off_date, string time_pick, string time_off, string locationID)
+        {
+            TempData["bookpickdate"] = book_pick_date;
+            TempData["bookoffdate"] = book_off_date;
+            TempData["timepick"] = time_pick;
+            TempData["timeoff"] = time_off;
+            TempData["locationID"] = locationID;
+            return RedirectToAction("Index", "RentACarList");
+        }
 
     }
 }
